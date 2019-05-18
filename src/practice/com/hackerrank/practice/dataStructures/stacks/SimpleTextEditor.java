7b87d2d4b42b75864bca9b0b189d92b1aa83f0fd
Ema Fazillah <gatsu2811@gmail.com>
2019-05-18 22:49:12 +0800

    Simple Text Editor (2nd commit)

--- src/practice/com/hackerrank/practice/dataStructures/stacks/SimpleTextEditor.java
+++ src/practice/com/hackerrank/practice/dataStructures/stacks/SimpleTextEditor.java
@@ -10,18 +10,30 @@ public class SimpleTextEditor {
 		Scanner scanner = new Scanner(System.in);
 		int N = scanner.nextInt();
 		int count = 0;
-		Stack<Integer> stack = new Stack<>();
+		Stack<String> stack = new Stack<>();
 		
 		while(count < N) {
 			int input = scanner.nextInt();
 			if(input == 1) {
-				
+				// Append string
+				stack.push(scanner.next());
 			} else if(input == 2) {
-				
+				// Delete the last kth character
+				int k = scanner.nextInt();
+				String inputString = stack.peek();
+				while(k > -1) {
+					inputString = inputString.substring(k - 1);
+					--k;
+				}
+				stack.push(inputString);
 			} else if(input == 3) {
-				
+				// Print the kth character
+				int k = scanner.nextInt();
+				char[] charArray = stack.peek().toCharArray();
+				System.out.println(charArray[k - 1]);
 			} else if(input == 4) {
-				
+				// Undo
+				stack.pop();
 			}
 			
 			++count;
